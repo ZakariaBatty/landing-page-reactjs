f91c03e9d291498da8217c0dfd81a5265955f9a3
ZakariaBAtty <zbatty0912@gmail.com>
2021-10-27 12:07:47 +0100

    add new section

--- src/Components/Button/SecondaryButton.js
+++ src/Components/Button/SecondaryButton.js
@@ -1,7 +1,32 @@
 import React from 'react';
+import styled from 'styled-components';
+import arrow from '../../img/arrow.svg';
 
-const SecondaryButton = () => {
-  return <div></div>;
+const SecondaryButton = ({ name }) => {
+  return (
+    <SecondaryButtonStyled>
+      {name}
+      <img src={arrow} alt="icon" />
+    </SecondaryButtonStyled>
+  );
 };
+
+const SecondaryButtonStyled = styled.button`
+  background-color: var(--dark-primary);
+  padding: 0.7rem 2rem;
+  font-family: inherit;
+  fony-size: inherit;
+  color: inherit;
+  border-radius: 20px;
+  outline: none;
+  border: none;
+  cursor: pointer;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  img {
+    padding-left: 0.9rem;
+  }
+`;
 
 export default SecondaryButton;
--- src/Components/Header/HeaderContent.js
+++ src/Components/Header/HeaderContent.js
@@ -1,23 +1,52 @@
 import React from 'react';
+import SecondaryButton from '../Button/SecondaryButton';
 import styled from 'styled-components';
+import phone from '../../img/phone.svg';
+import ring1 from '../../img/ring_orange.svg';
 
 const HeaderContent = () => {
   return (
     <HeaderContentStyled>
       <div className="left-content">
-        <h1>Smart banking for freelancers</h1>
-        <p className="white">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia
-          provident nulla numquam nisi obcaecati accusamus tempore saepe totam
-          dolorem odit necessitatibus impedit magni, iure repellat eaque
-          voluptates dolorum possimus corrupti.
-        </p>
+        <div className="left-text-container">
+          <h1>Smart banking for freelancers</h1>
+          <p className="white">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia
+            provident nulla numquam nisi obcaecati accusamus tempore saepe totam
+            dolorem odit necessitatibus impedit magni, iure repellat eaque
+            voluptates dolorum possimus corrupti.
+          </p>
+          <SecondaryButton name={'Register Now'} />
+        </div>
       </div>
-      <div className="right-content"></div>
+      <div className="right-content">
+        <img src={phone} alt="img" />
+        <img src={ring1} alt="icon" className="ring1" />
+      </div>
     </HeaderContentStyled>
   );
 };
 
-const HeaderContentStyled = styled.div``;
+const HeaderContentStyled = styled.div`
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);
+  padding-top: 3rem;
+  .left-content {
+    display: flex;
+    align-items: center;
+    padding-right: 6rem;
+    h1 {
+      font-size: 3rem;
+      font-weight: 600;
+    }
+    .white {
+      padding: 1.4rem 0;
+      line-height: 1.8rem;
+    }
+  }
+  .right-content {
+    position: relative;
+  }
+`;
 
 export default HeaderContent;
